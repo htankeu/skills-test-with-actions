cd2cf819ae5b20327fd029756f5e6224f04e3539
JoëlYvan Heda Tankeu <hedayvan@gmail.com>
2026-06-24 20:51:33 +0200

    reset tests to be true

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -13,7 +13,7 @@ from calculations import area_of_circle, get_nth_fibonacci   # noqa: E402
 def test_area_of_circle_positive_radius():
     """Test with a positive radius."""
     # Arrange
-    radius = 0
+    radius = 1
 
     # Act
     result = area_of_circle(radius)
